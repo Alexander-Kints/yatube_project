cb685b53dd5a1f84adc31d57d02bbbb987ec1f05
Александр Кинцлер <alexander.kintsler@yandex.ru>
2022-03-04 23:36:42 +0300

    Подключение bootstrap, настройка namespace

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,7 +5,11 @@ from django.http import HttpResponse
 
 def index(request):
     template = 'posts\index.html'
-    return render(request, template)
+    text = "Это главная страница проекта Yatube"
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
 
 
 def group_posts(request):
